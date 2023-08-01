b1d2dbd2eae3f19a7d85a5d5c4a24c76563c29a9
Arun Pillai <arundeepak89@gmail.com>
2023-07-31 20:12:08 -0400

    test with list

--- transformers/pipeline_img2text.py
+++ transformers/pipeline_img2text.py
@@ -2,5 +2,5 @@ from transformers import pipeline
 
 #captioner = pipeline(model="ydshieh/vit-gpt2-coco-en")
 captioner = pipeline(model="Salesforce/blip-image-captioning-base")
-res = captioner("https://huggingface.co/datasets/Narsil/image_dummy/raw/main/parrots.png")
+res = captioner(["https://huggingface.co/datasets/Narsil/image_dummy/raw/main/parrots.png", "https://free-images.com/sm/9596/dog_animal_greyhound_983023.jpg" ])
 print(res)
